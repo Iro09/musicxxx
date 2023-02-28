4b586fcab7836b4d38a03487982c6967c2cc317a
Pikachu <116916755+Iro09@users.noreply.github.com>
2023-02-28 21:00:47 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Iro09/IRO_X_MUSIC",
+    "https://github.com/Iro09/musicxxx",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
